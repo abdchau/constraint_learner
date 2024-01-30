bd98392908c8a94dec1b702c841919b9dad3ca65
abdchau <abdullahchaudhry0@outlook.com>
2024-01-30 16:52:34 +0100

    Prepare for instantiate_with_subtrees

--- implementation.py
+++ implementation.py
@@ -2,6 +2,7 @@
 Use this file to implement your solution. You can use the `main.py` file to test your implementation.
 """
 from itertools import product
+from fuzzingbook.Grammars import nonterminals
 
 def instantiate_with_nonterminals(constraint_pattern: str, nonterminals: list[str]) -> set[str]:
     num_of_nts = constraint_pattern.count("{}")
@@ -9,14 +10,22 @@ def instantiate_with_nonterminals(constraint_pattern: str, nonterminals: list[st
     
     return {constraint_pattern.format(*nts) for nts in cartesian}
 
+#---------------------------------------------------------------------------------------------------
+
 def instantiate_with_subtrees(abstract_constraint: str, nts_to_subtrees: dict) -> set[str]:
-    pass
+    print(nonterminals(abstract_constraint))
+
+#---------------------------------------------------------------------------------------------------
 
 def learn(constraint_patterns: list[str], derivation_trees: list) -> set[str]:
     pass
 
+#---------------------------------------------------------------------------------------------------
+
 def check(abstract_constraints: set[str], derivation_tree) -> bool:
     pass
+
+#---------------------------------------------------------------------------------------------------
 
 def generate(abstract_constraints: set[str], grammar: dict, produce_valid_sample: True) -> str:
     pass
